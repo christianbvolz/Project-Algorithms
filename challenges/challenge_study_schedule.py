ae0331995dfcc7d4a48c7d0568303c932efc6f04
Christian Volz <christianbvolz@gmail.com>
2022-10-13 18:22:18 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    try:
+        students = 0
+        for (entrance, exit) in permanence_period:
+            if entrance <= target_time <= exit:
+                students += 1
+    except TypeError or ValueError:
+        return None
+    else:
+        return students
