9884701d9ccef1786f240cf0d2d232b500310621
Christian Volz <christianbvolz@gmail.com>
2022-10-13 19:35:00 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+    for letter in first_string.lower():
+        if letter in second_string.lower():
+            second_string = second_string.lower().replace(letter, "", 1)
+        else:
+            return False
+    return True
